ee8aedbc725f9df0886b3a0f5d96b0d2cd9dffd4
Rob Loach <robloach@gmail.com>
2017-07-05 01:15:02 -0400

    Remove languages

--- libretro-scummvm.js
+++ libretro-scummvm.js
@@ -1,10 +1,10 @@
 const assert = require('assert')
 const fs = require('fs')
-const exec = require('child_process').execSync
 const crc = require('crc')
 const rimraf = require('rimraf')
-var sortObject = require('sort-object')
+const sortObject = require('sort-object')
 const pkg = require('./package.json')
+const exec = require('child_process').execSync
 
 // Clean the games directory.
 rimraf.sync('games')
@@ -24,7 +24,6 @@ var output = `clrmamepro (
 
 // Default set of unlisted games.
 var games = require('./games')
-var languages = require('./languages')
 
 // Build the .scummvm files.
 exec('scummvm --list-games')
@@ -38,8 +37,7 @@ exec('scummvm --list-games')
 	.sort()
 	// Loop through each one an make the .scummvm file
 	.forEach(function (line) {
-		var id = line.substring(0, 20)
-			.trim()
+		var id = line.substring(0, 20).trim()
 		if (id.length > 0) {
 			var title = line.substring(20)
 				.replace('/', ' - ')
@@ -69,38 +67,28 @@ for (var id in games) {
 		' CR': '\r'
 	}
 	for (var newlineType in newlineOptions) {
-		// Allow launching of specific languages
-		for (var lang in languages) {
-			// Cleanse both the language code and name
-			var langCode = ''
-			var langName = ''
-			if (languages[lang].length !== 0) {
-				langCode = '-' + lang
-				langName = ' ' + languages[lang]
-			}
-			// Construct the contents of the .scummvm file
-			var contents = id + langCode + newlineOptions[newlineType]
+		// Construct the contents of the .scummvm file
+		var contents = id + newlineOptions[newlineType]
 
-			// Write the file
-			fs.writeFileSync(`games/${name}${newlineType}${langName}.scummvm`, contents, {
-				encoding: 'ascii'
-			})
+		// Write the file
+		fs.writeFileSync(`games/${name}${newlineType}.scummvm`, contents, {
+			encoding: 'ascii'
+		})
 
-			// Calculate the CRC for the entry
-			var crcValue = crc.crc32(contents).toString(16)
+		// Calculate the CRC for the entry
+		var crcValue = crc.crc32(contents).toString(16)
 
-			// Diplay an output
-			console.log(name, newlineType, langName, '-', crcValue)
+		// Diplay an output
+		console.log(name, newlineType, '-', crcValue)
 
-			// Write the rom entry to the .DAT file.
-			output += `
+		// Write the rom entry to the .DAT file.
+		output += `
 game (
 	name "${name}"
 	description "${name}"
 `
-			output += `	rom ( name "${name}${newlineType}${langName}.scummvm" size ${contents.length} crc ${crcValue} )\n`
-			output += ')\n'
-		}
+		output += `	rom ( name "${name}${newlineType}.scummvm" size ${contents.length} crc ${crcValue} )\n`
+		output += ')\n'
 	}
 }
 
